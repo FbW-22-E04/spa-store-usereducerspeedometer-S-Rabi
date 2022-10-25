9d5e87cdbdae99b98eb8fac3ea2f89e26f19eee3
S Rabi <rabi.sanaye@yahoo.com>
2022-10-25 15:20:31 +0200

    c

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import ReactSpeedometer from "react-d3-speedometer";
+import { CarContext } from "./CarContext";
 
 export default function Car() {
-  return <div className="car">Make your instrument cluster here</div>;
+  const { state, dispatchState } = useContext(CarContext);
+
+  return (
+    <div
+      className="car"
+      style={{
+        textAlign: "center",
+        filter: state.started ? " " : "saturate(0%)",
+      }}
+    >
+      <ReactSpeedometer value={state.speed} height={160} />
+      <button onClick={() => dispatchState({ type: "toggleOnOff" })}>
+        ON/OFF
+      </button>
+      <button onClick={() => dispatchState({ type: "accelerate" })}>
+        Accelerate
+      </button>
+      <button onClick={() => dispatchState({ type: "brake" })}>Break</button>
+    </div>
+  );
 }
